b93ab2bb469fe126f4be8b9c90a14cc9198e8b19
patawat <patawat.wtk@gmail.com>
2017-03-13 20:36:23 +0700

    Add planet object

---
+++ ContentManagement/app/src/main/java/com/projectxi/berlemstudio/contentmanagement/res/Planet.java
@@ -0,0 +1,14 @@
+package com.projectxi.berlemstudio.contentmanagement.res;
+
+/**
+ * Created by patawat on 3/13/2017 AD.
+ */
+
+public class Planet extends Scene {
+
+    public Planet(String name, String des, String img_path, String scene){
+        super(name , des, img_path, scene);
+    }
+
+
+}
